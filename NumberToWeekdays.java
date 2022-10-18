0509cc2101e199ff0af3234c73931443d24218d6
Md. Nayeem Kazi <103841244+nayeem-kazi@users.noreply.github.com>
2022-10-18 12:52:21 +0600

    First Commit

---
+++ NumberToWeekdays.java
@@ -0,0 +1,30 @@
+package com.exam;
+
+import java.util.Scanner;
+
+public class NumberToWeekdays {
+
+    public static void main(String[] args) {
+        int n = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a numbe to see the weekday's name : ");
+        n = input.nextInt();
+        
+        switch(n) {
+            case 1: System.out.println("Saturday");
+            break;
+            case 2: System.out.println("Sunday");
+            break;
+            case 3: System.out.println("Monday");
+            break;
+            case 4: System.out.println("Tuesday");
+            break;
+            case 5: System.out.println("Wednesday");
+            break;
+            case 6: System.out.println("Thursday");
+            break;
+            case 7: System.out.println("Friday");
+            break;
+        }
+    }
+}
